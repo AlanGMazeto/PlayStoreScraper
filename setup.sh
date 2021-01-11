d3de6268d9057619dececa6387d6244569fa0978
Alan Mazeto <alan.geovani.mazeto@gmail.com>
2021-01-11 00:22:44 -0300

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 mkdir -p ~/.streamlit/echo "\
 [general]\n\
 email = \"alan.geovani.mazeto@gmail.com\"\n\
-" > ~/.streamlit/credentials.tomlecho "\
+" > ~/.streamlit/credentials.toml
+
+echo "\
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
-port = 5000\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
